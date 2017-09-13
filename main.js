591c1f5dd75fad5c5fb3120bdb29cb8ed13d3e84
markjconlon <markjconlon@gmail.com>
2017-09-13 19:11:11 -0400

    mapping the bids and asks from liqui's returned json to the buys and sells respectively

--- main.js
+++ main.js
@@ -6,7 +6,20 @@ document.addEventListener('DOMContentLoaded', () => {
   const liquiPromise = fetch('https://api.liqui.io/api/3/depth/eth_btc?limit=10', { mode: 'cors' });
   liquiPromise
     .then(data => data.json())
-    .then(data => console.log(data))
+    .then((data) => {
+      const bids = (data.eth_btc.bids).map(info => `${info[0]}BTC | QTY = ${info[1]}`);
+      for (let i = 0; i < bids.length; i++) {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(bids[i]));
+        liquiBuys.appendChild(li);
+      }
+      const asks = (data.eth_btc.asks).map(info => `${info[0]}BTC | QTY = ${info[1]}`);
+      for (let i = 0; i < asks.length; i++) {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(asks[i]));
+        liquiSells.appendChild(li);
+      }
+    })
     .catch((err) => {
       console.log('Whoops');
       console.log(err);
