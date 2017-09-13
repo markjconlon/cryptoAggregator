5d988c5a17493356e96e55dffc5fca68760984e5
markjconlon <markjconlon@gmail.com>
2017-09-13 18:54:00 -0400

    created main.js, added to index script tags

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const krackenBuys = document.querySelector('#KB');
+  const krackenSells = document.querySelector('#KS');
+  const liquiBuys = document.querySelector('#LB');
+  const liquiSells = document.querySelector('#LS');
+});
