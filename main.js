ad1bafaf58800fe77660ba2bc2eb27e549f048cb
markjconlon <markjconlon@gmail.com>
2017-09-13 19:07:31 -0400

    added fetch for liqui's api then logged to console

--- main.js
+++ main.js
@@ -3,4 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const krackenSells = document.querySelector('#KS');
   const liquiBuys = document.querySelector('#LB');
   const liquiSells = document.querySelector('#LS');
+  const liquiPromise = fetch('https://api.liqui.io/api/3/depth/eth_btc?limit=10', { mode: 'cors' });
+  liquiPromise
+    .then(data => data.json())
+    .then(data => console.log(data))
+    .catch((err) => {
+      console.log('Whoops');
+      console.log(err);
+    });
 });
