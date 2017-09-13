8be5e51c9d4625e55cc291e95aa1ea0fe105e278
markjconlon <markjconlon@gmail.com>
2017-09-13 19:17:28 -0400

    switched all kracken to kraken as that is the correct spelling of the exchange

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const krackenBuys = document.querySelector('#KB');
-  const krackenSells = document.querySelector('#KS');
+  const krakenBuys = document.querySelector('#KB');
+  const krakenSells = document.querySelector('#KS');
   const liquiBuys = document.querySelector('#LB');
   const liquiSells = document.querySelector('#LS');
   const liquiPromise = fetch('https://api.liqui.io/api/3/depth/eth_btc?limit=10', { mode: 'cors' });
@@ -23,9 +23,9 @@ document.addEventListener('DOMContentLoaded', () => {
     .catch((err) => {
       console.log(err);
     });
-// using cors-proxy as a temporary work around
-  const krackenPromise = fetch('http://cors-proxy.htmldriven.com/?url=https://api.kraken.com/0/public/Depth?pair=XETHXXBT&count=10');
-  krackenPromise
+  // using cors-proxy as a temporary work around
+  const krakenPromise = fetch('http://cors-proxy.htmldriven.com/?url=https://api.kraken.com/0/public/Depth?pair=XETHXXBT&count=10');
+  krakenPromise
     .then(response => response.json())
     .then((response) => {
       console.log(response);
